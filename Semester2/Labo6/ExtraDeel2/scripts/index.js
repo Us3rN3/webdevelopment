4425017c1ea2b8352c0dd7ba5210b0f3661a103c
Us3rN3 <105727521+Us3rN3@users.noreply.github.com>
2024-03-24 11:25:17 +0100

    d

--- Semester2/Labo6/ExtraDeel2/scripts/index.js
+++ Semester2/Labo6/ExtraDeel2/scripts/index.js
@@ -1,5 +1,16 @@
-const setup = () =>{
+document.addEventListener("DOMContentLoaded", function() {
+    let listItems = document.querySelectorAll("li");
+    listItems.forEach(function(item) {
+        item.classList.add("listitem");
+    });
 
-}
+    let style = document.createElement("style");
+    style.textContent = ".listitem { color: red; }";
+    document.head.appendChild(style);
+});
 
-window.addEventListener("load", setup)
+document.addEventListener("DOMContentLoaded", function() {
+    let img = document.createElement("img");
+    img.src = "../img/1038860.jpg";
+    document.body.appendChild(img);
+});
