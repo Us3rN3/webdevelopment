a5b6302bb4de6e6a0e57b305badc8e809b814392
Us3rN3 <quinten.verbeke@outlook.com>
2024-04-25 21:44:01 +0200

    labo9

--- Semester2/Labo9/ColorpickerUpdate/scripts/code.js
+++ Semester2/Labo9/ColorpickerUpdate/scripts/code.js
@@ -1,20 +1,36 @@
 const initialize = () => {
 	let sliders = document.getElementsByClassName("slider");
+
 	for (let i = 0; i < sliders.length; i++) {
 		sliders[i].addEventListener("change", update);
 		sliders[i].addEventListener("input", update);
 	}
-	update();
+
+	let savedSettings = localStorage.getItem("settingColorpicker");
+	if (savedSettings) {
+		let settings = JSON.parse(savedSettings);
+		document.getElementById("sldRed").value = settings.red;
+		document.getElementById("sldGreen").value = settings.green;
+		document.getElementById("sldBlue").value = settings.blue;
+		update();
+	} else {
+		update(); // If no saved settings, apply default
+	}
+
+	document.getElementById("button").addEventListener("click", saveSettings)
 };
 
 const update = () => {
 	let red = document.getElementById("sldRed").value;
 	let green = document.getElementById("sldGreen").value;
 	let blue = document.getElementById("sldBlue").value;
+
 	document.getElementById("lblRed").innerHTML = red;
 	document.getElementById("lblGreen").innerHTML = green;
 	document.getElementById("lblBlue").innerHTML = blue;
+
 	let swatch = document.getElementById("swatch");
+
 	swatch.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
@@ -29,15 +45,37 @@ const saveColor = () => {
 
 	let closeButton = document.createElement("span");
 	closeButton.textContent = "\u00D7";
+
 	closeButton.classList.add("closeButton");
+
 	closeButton.addEventListener("click", function() {
 		savedColor.remove();
 	});
+
 	savedColor.appendChild(closeButton);
 
 	document.getElementById("savedColors").appendChild(savedColor);
+
+	savedColor.addEventListener("click", () => {
+		document.getElementById("sldRed").value = red;
+		document.getElementById("sldGreen").value = green;
+		document.getElementById("sldBlue").value = blue;
+		update();
+	})
+
 };
 
+const saveSettings = () => {
+	let settings = {};
+	let settingsJSON;
+
+	settings.red = parseInt(document.getElementById("sldRed").value);
+	settings.green = parseInt(document.getElementById("sldGreen").value);
+	settings.blue = parseInt(document.getElementById("sldRed").value);
+
+	settingsJSON = JSON.stringify(settings);
+	localStorage.setItem("settingColorpicker", settingsJSON);
+}
 
 
 window.addEventListener("load", initialize);
